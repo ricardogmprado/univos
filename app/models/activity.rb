d480e5f31c118e07c72253e3735085c3f15e1db5
Ricardo Prado <ricardogmprado@gmail.com>
2019-06-05 11:48:05 +0800

    Chat is working + Button Join Activity appear on Activity Show only if appointment is not confirmed yet

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments
+  has_many :messages, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
   validates :date, presence: true
