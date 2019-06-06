44b5c5bf67b6401bd99a8d348d880f598a882b78
Roland Rether <rolandmhcontact@gmail.com>
2019-06-06 16:04:40 +0800

    autocomplete in activity new view is working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,5 +19,7 @@ import 'bootstrap';
 
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
+initAutocomplete();
