9c6f3b2553ed3230f9572c93adb752f89ed57645
Ricardo Prado <ricardogmprado@gmail.com>
2019-06-07 11:26:56 +0800

    form is not getting all the info from fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,28 +13,17 @@ class User < ApplicationRecord
   mount_uploader :photo, UserPhotoUploader
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  def self.find_for_facebook_oauth(auth)
-    user_params = auth.slice("provider", "uid")
-    user_params.merge! auth.info.slice("email", "first_name", "last_name")
-    user_params[:facebook_picture_url] = auth.info.image
-    user_params[:token] = auth.credentials.token
-    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
-    user_params = user_params.to_h
-
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
-    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
-    if user
-      user.update(user_params)
-    else
-      user = User.new(user_params)
-      user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      user.save
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name: auth.extra.raw_info.first_name + '' + auth.extra.raw_info.last_name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20],
+                           photo: auth.info.image
+                           )
     end
-
-    return user
-  end
-
-  def fullname(first_name, last_name)
-    name = first_name + '' + last_name
+    user
   end
 end
