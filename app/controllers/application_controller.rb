9c7b3ae0a0f1483df353b9c4272e35095b3c5342
Ricardo Prado <ricardogmprado@gmail.com>
2019-06-26 10:33:48 +0800

    adding meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,9 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def default_url_options
+  { host: ENV["https://univos.herokuapp.com"] || "localhost:3000" }
+end
+
 end
